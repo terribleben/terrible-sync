86baffe5a6cf9fa80e0da6f22bcd54db510e6736
Ben Roth <spongebucket@gmail.com>
2015-07-30 15:17:12 -0700

    tap tempo works

--- terrible-sync/TSViewController.m
+++ terrible-sync/TSViewController.m
@@ -8,11 +8,21 @@
 
 #import "TSViewController.h"
 
+#define TS_NUM_BEATS_VIEWS 3
+#define TS_MIN_BPM 60.0f
+#define TS_MAX_BPM 480.0f
+
 @interface TSViewController ()
+{
+    NSTimeInterval dtmLastTap;
+}
 
 @property (nonatomic, strong) UIButton *btnTap;
+@property (nonatomic, strong) UIView *vBeat;
+
 @property (nonatomic, strong) NSTimer *tmrBeat;
 
+- (void)tap;
 - (void)stopBeat;
 - (void)startBeatWithDuration: (NSTimeInterval)duration;
 - (void)beat;
@@ -21,14 +31,30 @@
 
 @implementation TSViewController
 
+- (id)init
+{
+    if (self = [super init]) {
+        dtmLastTap = 0;
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
     
+    // beats views
+    self.vBeat = [[UIView alloc] init];
+    _vBeat.backgroundColor = [UIColor whiteColor];
+    _vBeat.clipsToBounds = YES;
+    [self.view addSubview:_vBeat];
+    
     // the button
     self.btnTap = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_btnTap addTarget:self action:@selector(tap) forControlEvents:UIControlEventTouchUpInside];
     _btnTap.clipsToBounds = YES;
+    _btnTap.backgroundColor = [UIColor blackColor];
     _btnTap.frame = CGRectMake(0, 0, 192.0f, 192.0f);
     _btnTap.layer.borderWidth = 6.0f / [UIScreen mainScreen].scale;
     _btnTap.layer.borderColor = [UIColor redColor].CGColor;
@@ -44,16 +70,30 @@
 {
     [super viewWillLayoutSubviews];
     _btnTap.center = CGPointMake(CGRectGetMidX(self.view.bounds), self.view.bounds.size.height * 0.4f);
+    _vBeat.frame = _btnTap.frame;
+    _vBeat.layer.cornerRadius = _btnTap.layer.cornerRadius;
 }
 
 
 #pragma mark internal
 
+- (void)tap
+{
+    NSTimeInterval now = [[NSDate date] timeIntervalSince1970];
+    NSTimeInterval sinceLastTap = now - dtmLastTap;
+    CGFloat bpm = 60.0f / sinceLastTap;
+    if (bpm >= TS_MIN_BPM && bpm <= TS_MAX_BPM) {
+        [self startBeatWithDuration:sinceLastTap];
+    }
+    
+    dtmLastTap = now;
+}
+
 - (void)stopBeat
 {
     if (_tmrBeat) {
-        _tmrBeat = nil;
         [_tmrBeat invalidate];
+        _tmrBeat = nil;
     }
 }
 
@@ -67,7 +107,11 @@
 
 - (void)beat
 {
-    NSLog(@"beat");
+    _vBeat.transform = CGAffineTransformIdentity;
+    _vBeat.transform = CGAffineTransformMakeScale(1.1f, 1.1f);
+    [UIView animateWithDuration:0.1f animations:^{
+        _vBeat.transform = CGAffineTransformIdentity;
+    }];
 }
 
 @end
